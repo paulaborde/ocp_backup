0edc3547ce7bc6ac56261533fe3e1dc5b5e88843
U-DCT\PDSZ779 <PDSZ779@311810SLB3I.dct.adt.local>
2020-09-17 11:19:05 +0200

    add script

---
+++ backup_workshop.py
@@ -0,0 +1,22 @@
+import os
+
+# Generate lab commands
+commands = []
+file = open("raw.txt", "r")
+for line in file:
+    if "[student@workstation ~]$ lab" in line and "start" in line:
+        commands.append(line.split('$')[1][1:])
+
+# for each lab create repo
+for c in commands:
+    print(c)
+    repo_name = "OCP_" + c.split(" ")[1]
+    github_creation = "curl -F 'login=paulaborde' -F 'token=418f9cf634f53b49417acfa9ed220cace7a771f7' https://github.com/api/v2/yaml/repos/create -F name=%s" % repo_name
+    os.system(github_creation)
+    os.system("cd DO380")
+    os.system("git add *")
+    os.system("git commit -am 'init data in repo'")
+    os.system("git remote add origin https://github.com/paulaborde/%s.git" % repo_name)
+    os.system("git push")
+    os.system("cd ")
+    os.system(c.replace('start', 'finish'))
